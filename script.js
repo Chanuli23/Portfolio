6ca0133750a38292f2190548813220550938405c
TDCS Gunawadane <39-bit-0036@kdu.ac.lk>
2023-10-17 10:55:47 +0530

    updates

--- script.js
+++ script.js
@@ -15,12 +15,6 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
 
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
